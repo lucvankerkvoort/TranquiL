ad1f055ba8e2c330f50895d35ba43acd6e6402e8
Tangerinez <gtang159@gmail.com>
2019-06-19 17:03:40 -0700

    Added pseudocode for user authentication and login as well as possible search bar functionality.

--- app/public/logic.js
+++ app/public/logic.js
@@ -2,17 +2,48 @@ var profiles = require("./user-profiles");
 
 var userInput = {
   name: "",
+  password: "",
   category: "",
   data: []
 };
 
 var mockName = "Luc";
+var mockPassword = "Tang";
 var mockCategory = "yoga";
 var mockInput = [5, 7, 9, 5, 4];
 
 userInput.data = mockInput;
 userInput.category = mockCategory;
 userInput.name = mockName;
+userInput.password = mockPassword;
+
+/* PSEUDOCODED USER AUTHENTICATION (GREG) ////////////////
+// (For sign-up flow)
+// 1. Capture user's inputs from the survey for proposed username and password
+//        - Parameters:
+//                - Username is certain set of characters
+//                - Password meets length and also character requirements
+//                - If current username already exists then user must choose another username
+// 2. If username and password are BOTH valid, then the new user's data will get stored in our database
+// 3. Once user fills out the form, user is automatically logged in and their results are generated
+
+// (For existing users that are logging in)
+// 1. Capture's user's inputs from login flow and compares it to existing user IDs usernames and passwords in the database
+//      - If username and password do not match, error message displayed below input bars stating to retype info
+// 2. If username and password match, user is logged in and results from previous form input is displayed
+*/
+
+/* PSEUDOCODED SEARCH BAR FUNCTIONALITY (GREG) (NICE-TO-HAVE)//////////////
+// 1. Search Bar would exist at top of the page
+// 2. Users can filter through categories in the search bar
+//         - Categories: Yoga, Meditation, Exercise
+// 3. Conditions for the filter
+//         - Filter would take inputs as strings
+//         - Filter would check for matching sequences between the user's inputs and the values in that category in the database
+//         - Filtered search would render below the search bar in a box that contains the options
+//         - Options would be dynamic and would maybe be able to be clicked so that they would get appended in the right section
+//         - Filter would contain a reset button as well
+*/
 
 function scoreCalculator(userInput) {
   var score = 0;
