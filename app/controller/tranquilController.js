1a8f214ee4d101aa1ea0d03fa42f3935ba928b13
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-06-22 13:21:12 -0700

    created the controller and the server file

---
+++ app/controller/tranquilController.js
@@ -0,0 +1,23 @@
+// We require both the models
+var userInfo = require("../models/user_info");
+var dataOutput = require("../models/data_output");
+
+var path = require("path");
+// We require the express module
+var express = require("express");
+
+// We require the Router method from express
+var router = express.Router();
+
+// We set the route for our home page
+router.get("/", function(req, res) {
+  res.send(path.join(__dirname, "../views/main.html"));
+  // res.render("index") if we need handlebars
+});
+
+// We set the route for our first page
+router.get("/survey/q1", function(req, res) {
+  res.send(path.join(__dirname, "../views/survey.html"));
+});
+
+module.exports = router;
--- app/db/schema.sql
+++ app/db/schema.sql
@@ -1,4 +1,3 @@
-DROP DATABASE IF EXISTS tranquil_db;
 CREATE DATABASE tranquil_db;
 
 USE tranquil_db;
