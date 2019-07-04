8deb9691275249b806be3b3590d1397fcf6e9455
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-07-04 11:04:35 -0700

    I made changes to the connection document to make it ready for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "yourRootPassword",
-  database: "tranquil_db"
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "yourRootPassword",
+    database: "tranquil_db"
+  });
+}
 connection.connect(function(err) {
   if (err) throw err;
   console.log("you are connected through " + connection.threadId);
